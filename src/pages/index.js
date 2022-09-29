bb4f7bafe0b6356fdcfd9053aea63d68bd545fd6
sarah <skg@neofilm.us>
2022-09-29 13:20:03 -0400

    Use StaticImage component on site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,19 @@
-// Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
-// Step 2: Define component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   )
 }
 
-// More to come, just copy for now
 export const Head = () => <title>Home Page</title>
 
-// Step 3: Export component
 export default IndexPage
