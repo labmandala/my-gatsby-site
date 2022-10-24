780f970db5225908d94a211f3940c96e1d1d456b
sarah <skg@neofilm.us>
2022-10-24 16:07:15 -0400

    Define and export page query built in GraphiQL

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,10 +6,20 @@ import Seo from '../components/seo'
 const BlogPage = () => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My posts will go in here</p>
+      <p>My cool posts will go in here</p>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export const Head = () => <Seo title="My Blog Posts" />
 
