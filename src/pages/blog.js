979991f03561078d308dece1d384a905f4e9512d
sarah <skg@neofilm.us>
2022-10-26 15:20:16 -0400

    Iterate over nodes array & render posts w/ .map()

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,10 +3,18 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>My cool posts will go in here</p>
+      <ul>
+      {
+        data.allFile.nodes.map(node => (
+          <li key={node.name}>
+            {node.name}
+          </li>
+        ))
+      }
+      </ul>
     </Layout>
   )
 }
